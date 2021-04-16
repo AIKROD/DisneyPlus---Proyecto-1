0d324206ebdc4fa99c615529990347c410aec60d
AIKROD <krodriguezv3@miumg.edu.gt>
2021-04-16 13:40:07 -0600

    u

--- DisneyPlus - Proyecto 1/Feed.Designer.cs
+++ DisneyPlus - Proyecto 1/Feed.Designer.cs
@@ -41,6 +41,7 @@ namespace DisneyPlus___Proyecto_1
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.btnVerPeli = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.gbPeliculaLista.SuspendLayout();
             this.SuspendLayout();
@@ -54,12 +55,14 @@ namespace DisneyPlus___Proyecto_1
             columnHeader1,
             this.columnHeader2});
             this.listView1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.listView1.GridLines = true;
             this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(12, 180);
+            this.listView1.Location = new System.Drawing.Point(30, 425);
+            this.listView1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(609, 122);
+            this.listView1.Size = new System.Drawing.Size(1624, 291);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listView1_ItemSelectionChanged);
@@ -68,9 +71,10 @@ namespace DisneyPlus___Proyecto_1
             // btnAgregarList
             // 
             this.btnAgregarList.Enabled = false;
-            this.btnAgregarList.Location = new System.Drawing.Point(405, 47);
+            this.btnAgregarList.Location = new System.Drawing.Point(1080, 112);
+            this.btnAgregarList.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.btnAgregarList.Name = "btnAgregarList";
-            this.btnAgregarList.Size = new System.Drawing.Size(165, 26);
+            this.btnAgregarList.Size = new System.Drawing.Size(440, 62);
             this.btnAgregarList.TabIndex = 1;
             this.btnAgregarList.Text = "Agregar a mi Lista";
             this.btnAgregarList.UseVisualStyleBackColor = true;
@@ -84,9 +88,11 @@ namespace DisneyPlus___Proyecto_1
             this.gbPeliculaLista.Controls.Add(this.label3);
             this.gbPeliculaLista.Controls.Add(this.label2);
             this.gbPeliculaLista.Controls.Add(this.label1);
-            this.gbPeliculaLista.Location = new System.Drawing.Point(64, 32);
+            this.gbPeliculaLista.Location = new System.Drawing.Point(171, 76);
+            this.gbPeliculaLista.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.gbPeliculaLista.Name = "gbPeliculaLista";
-            this.gbPeliculaLista.Size = new System.Drawing.Size(222, 112);
+            this.gbPeliculaLista.Padding = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.gbPeliculaLista.Size = new System.Drawing.Size(592, 267);
             this.gbPeliculaLista.TabIndex = 2;
             this.gbPeliculaLista.TabStop = false;
             this.gbPeliculaLista.Text = "Película";
@@ -94,74 +100,92 @@ namespace DisneyPlus___Proyecto_1
             // lblCategoriaLista
             // 
             this.lblCategoriaLista.AutoSize = true;
-            this.lblCategoriaLista.Location = new System.Drawing.Point(106, 79);
+            this.lblCategoriaLista.Location = new System.Drawing.Point(283, 188);
+            this.lblCategoriaLista.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.lblCategoriaLista.Name = "lblCategoriaLista";
-            this.lblCategoriaLista.Size = new System.Drawing.Size(0, 13);
+            this.lblCategoriaLista.Size = new System.Drawing.Size(0, 32);
             this.lblCategoriaLista.TabIndex = 5;
             // 
             // lblAnioLista
             // 
             this.lblAnioLista.AutoSize = true;
-            this.lblAnioLista.Location = new System.Drawing.Point(106, 51);
+            this.lblAnioLista.Location = new System.Drawing.Point(283, 122);
+            this.lblAnioLista.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.lblAnioLista.Name = "lblAnioLista";
-            this.lblAnioLista.Size = new System.Drawing.Size(0, 13);
+            this.lblAnioLista.Size = new System.Drawing.Size(0, 32);
             this.lblAnioLista.TabIndex = 4;
             // 
             // lblNombreLista
             // 
             this.lblNombreLista.AutoSize = true;
-            this.lblNombreLista.Location = new System.Drawing.Point(106, 22);
+            this.lblNombreLista.Location = new System.Drawing.Point(283, 52);
+            this.lblNombreLista.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.lblNombreLista.Name = "lblNombreLista";
-            this.lblNombreLista.Size = new System.Drawing.Size(0, 13);
+            this.lblNombreLista.Size = new System.Drawing.Size(0, 32);
             this.lblNombreLista.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 79);
+            this.label3.Location = new System.Drawing.Point(77, 188);
+            this.label3.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.Size = new System.Drawing.Size(147, 32);
             this.label3.TabIndex = 2;
             this.label3.Text = "Categoría:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(30, 51);
+            this.label2.Location = new System.Drawing.Point(80, 122);
+            this.label2.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.Size = new System.Drawing.Size(74, 32);
             this.label2.TabIndex = 1;
             this.label2.Text = "Año:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(30, 22);
+            this.label1.Location = new System.Drawing.Point(80, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.Size = new System.Drawing.Size(123, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre:";
             // 
             // btnVerPeli
             // 
             this.btnVerPeli.Enabled = false;
-            this.btnVerPeli.Location = new System.Drawing.Point(405, 91);
+            this.btnVerPeli.Location = new System.Drawing.Point(1080, 217);
+            this.btnVerPeli.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.btnVerPeli.Name = "btnVerPeli";
-            this.btnVerPeli.Size = new System.Drawing.Size(165, 26);
+            this.btnVerPeli.Size = new System.Drawing.Size(440, 62);
             this.btnVerPeli.TabIndex = 3;
             this.btnVerPeli.Text = "Ver Película";
             this.btnVerPeli.UseVisualStyleBackColor = true;
             this.btnVerPeli.Click += new System.EventHandler(this.btnVerPeli_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(30, 753);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1624, 178);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
             // Feed
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(633, 314);
+            this.ClientSize = new System.Drawing.Size(1893, 1329);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnVerPeli);
             this.Controls.Add(this.gbPeliculaLista);
             this.Controls.Add(this.btnAgregarList);
             this.Controls.Add(this.listView1);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "Feed";
             this.Text = "Feed";
             this.Load += new System.EventHandler(this.Feed_Load);
@@ -184,5 +208,6 @@ namespace DisneyPlus___Proyecto_1
         private System.Windows.Forms.Label lblAnioLista;
         private System.Windows.Forms.Label lblNombreLista;
         private System.Windows.Forms.Button btnVerPeli;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
--- DisneyPlus - Proyecto 1/Feed.cs
+++ DisneyPlus - Proyecto 1/Feed.cs
@@ -44,21 +44,96 @@ namespace DisneyPlus___Proyecto_1
 
         private void RecorrerLista_feed(Lista lst, string grupo)
         {
-            ListViewGroup lvg = new ListViewGroup();
-            lvg.Name = grupo;
-            lvg.Header = grupo;
-            listView1.Groups.Add(lvg);
+            //ListViewGroup lvg = new ListViewGroup();
+            //lvg.Name = grupo;
+            //lvg.Header = grupo;
+            
+            //listView1.Groups.Add(lvg);
+
+            ListView lvCategory = new ListView();
+            lvCategory.View = View.LargeIcon;
+            lvCategory.BorderStyle = BorderStyle.None;
+            lvCategory.Cursor = Cursors.Hand;//Set the mouse to hand
+
+            //lvCategory.Left = 430;
+            lvCategory.Width = 590;
+            //lvCategory.Dock = (DockStyle.Right && Dock );
+            //lvCategory.Dock = DockStyle.Left;
+            //lvCategory.Dock = DockStyle.;
+            //lvCategory.Dock = DockStyle.Right;
+
+            lvCategory.Height = 55;
+            lvCategory.Top = 15;
+            //lvCategory.Bottom = 10;
+            lvCategory.Left = 10;
+            //lvCategory.Right = 10;
+            lvCategory.Alignment = ListViewAlignment.Left;
+            
+            //lvCategory.Anchor = AnchorStyles.Right;
+            //lvCategory.BackColor = Color.lFromArgb(216, 222, 230);//Background color
+            lvCategory.SelectedIndexChanged += new EventHandler(lvCategory_SelectedIndexChanged);
+            lvCategory.View = View.Tile;
+
 
             for (int i = 0; i < lst.NumeroElementos; i++)
             {
                 Pelicula pelicula = (Pelicula)lst.complementarRecorrido();
                 ListViewItem lvi = new ListViewItem();
                 lvi.Text = pelicula.nombre;
-                lvi.Group = lvg;
+                //lvi.EnsureVisible();
 
-                listView1.Items.Add(lvi);
+                //listView1.Items.Add(lvi);
+
+
+                //Add a click event
+                //lvCategory.SelectedIndexChanged += new EventHandler(lvCategory_SelectedIndexChanged);
+                //lvCategory.Groups.Add(lvg);
+                lvCategory.Items.Add(lvi);
+
             }
             lst.ReiniciarActual();
+            groupBox1.Controls.Add(lvCategory);
+
+        }
+
+        private void lvCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListView lvCategory = (ListView)sender;//Get dynamically created listview object
+
+
+            if (lvCategory.SelectedItems.Count > 0)
+            {
+                btnVerPeli.Enabled = true;
+                btnAgregarList.Enabled = true;
+                //if (lvCategory.SelectedItems[0].ToString() == "Disney")
+                //{
+                    pelicula = buscarPelicula(frm1.Disney, lvCategory.SelectedItems[0].Text.ToString());
+                //}
+                //else if (lvCategory.SelectedItems[0].ToString() == "Pixar")
+                //{
+                //    pelicula = buscarPelicula(frm1.Pixar, lvCategory.SelectedItems[0].Text.ToString());
+                //}
+                //else if (lvCategory.SelectedItems[0].ToString() == "Marvel")
+                //{
+                //    pelicula = buscarPelicula(frm1.Marvel, lvCategory.SelectedItems[0].Text.ToString());
+                //}
+                //else if (lvCategory.SelectedItems[0].ToString() == "Star Wars")
+                //{
+                //    pelicula = buscarPelicula(frm1.Star_Wars, lvCategory.SelectedItems[0].Text.ToString());
+                //}
+                //else if (lvCategory.SelectedItems[0].ToString() == "National Geographic")
+                //{
+                //    pelicula = buscarPelicula(frm1.National_Geographic, lvCategory.SelectedItems[0].Text.ToString());
+                //}
+                //else
+                //{
+                //    pelicula = null;
+                //}
+
+                lblNombreLista.Text = pelicula.nombre;
+                lblAnioLista.Text = pelicula.año.ToString();
+                lblCategoriaLista.Text = pelicula.categoria.ToString();
+            }
         }
 
         private void btnAgregarList_Click(object sender, EventArgs e)
